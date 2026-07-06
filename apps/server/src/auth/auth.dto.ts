7fb4bf05203d0be0310aac34feced74aa7f844ce
Vikhyat Sharma <vikhyats93@gmail.com>
2026-07-06 19:49:28 +0530

    chore: adding auth dto

---
+++ apps/server/src/auth/auth.dto.ts
@@ -0,0 +1,42 @@
+import { IsEmail, IsString, MinLength, IsOptional } from 'class-validator';
+
+export class RegisterDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(8)
+  password: string;
+}
+
+export class LoginDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  password: string;
+}
+
+export class GoogleAuthDto {
+  @IsString()
+  idToken: string;
+}
+
+export class PasswordResetRequestDto {
+  @IsEmail()
+  email: string;
+}
+
+export class PasswordResetConfirmDto {
+  @IsString()
+  token: string;
+
+  @IsString()
+  @MinLength(8)
+  newPassword: string;
+}
+
+export class RefreshTokenDto {
+  @IsString()
+  refreshToken: string;
+}
