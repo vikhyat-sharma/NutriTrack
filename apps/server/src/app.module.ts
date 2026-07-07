f9cf94d72b41215004f0a4d2a63c16fe0dfea85c
Vikhyat Sharma <vikhyats93@gmail.com>
2026-07-07 20:54:36 +0530

    chore: adding app module

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
-import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { AssistantModule } from './assistant/assistant.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { MealsModule } from './meals/meals.module';
+import { NutritionModule } from './nutrition/nutrition.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    UsersModule,
+    AuthModule,
+    NutritionModule,
+    MealsModule,
+    DashboardModule,
+    AssistantModule,
+  ],
 })
 export class AppModule {}
