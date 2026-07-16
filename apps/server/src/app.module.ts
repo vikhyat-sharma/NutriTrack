6a6a83ce55842fcce6e59104d215f3ee9bfa7216
Vikhyat Sharma <vikhyats93@gmail.com>
2026-07-17 00:42:09 +0530

    chore: adding app module for app server

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import * as Joi from 'joi';
 import { AuthModule } from './auth/auth.module';
 import { AssistantModule } from './assistant/assistant.module';
 import { DashboardModule } from './dashboard/dashboard.module';
@@ -10,7 +13,18 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().required(),
+        JWT_SECRET: Joi.string().min(32).required(),
+        JWT_REFRESH_SECRET: Joi.string().min(32).required(),
+        PORT: Joi.number().default(3000),
+        CORS_ORIGIN: Joi.string().default(''),
+        GOOGLE_CLIENT_ID: Joi.string().optional().allow(''),
+      }),
+    }),
+    ThrottlerModule.forRoot([{ ttl: 60_000, limit: 60 }]),
     PrismaModule,
     UsersModule,
     AuthModule,
@@ -19,5 +33,6 @@ import { UsersModule } from './users/users.module';
     DashboardModule,
     AssistantModule,
   ],
+  providers: [{ provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
 export class AppModule {}
